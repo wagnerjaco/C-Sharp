5346f1b8c3c6022df7dccf830f6500771351ee14
wagnerjaco <wagnerjaco@gmail.com>
2023-06-29 14:11:41 -0300

    filtro de busca
    filtro de busca

--- Front/ProEvernto-App/src/app/eventos/eventos.component.ts
+++ Front/ProEvernto-App/src/app/eventos/eventos.component.ts
@@ -11,7 +11,24 @@ export class EventosComponent {
   widthImg: number =200;
   marginImg: number = 2;
   showImg: boolean = true;
-  filtrolista: string = '';
+  private _filtrolista: string = '';
+
+  public get filtrolista(){
+    return this._filtrolista;
+
+  }
+
+  public set filtrolista(value: string){
+    this._filtrolista =value;
+    this.eventos = this.filtrolista ? this.filtrarEventos(this.filtrolista) : this.eventos ;
+  }
+
+  filtrarEventos (filtrarPor : string) : any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (      evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!== -1
+    )
+  }
 
 
 
